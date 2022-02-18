638f6aa048e8d75ff1e9e0b18b788f8de8af3e58
Breno Henrique <breno.henrique@aluno.ifsp.edu.br>
2022-02-18 13:21:19 -0300

    Build before deploy 3
    Responsive CSS made using flexbox

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,28 +4,36 @@ import React from 'react'
 export default function Navbar() {
   return (
     <nav>
+        <div class="logo">
+          <h3>&lt;brenopelegrin/&gt;</h3>
+        </div>
         <link rel="stylesheet" href="//use.fontawesome.com/releases/v5.0.7/css/all.css"/>
-        <h3>&lt;brenopelegrin/&gt;</h3>
-        <div className="links">
+        <div class="links">
           <ul>
-            <li><Link to="/">Início</Link></li>
-            <li><Link to="/about">Currículo</Link></li>
-            <li><Link to="/projects">Projetos</Link></li>
-            <li><Link to="/blog">Blog</Link></li>
-            <li><Link to="#"><i class="fa fa-globe fa-fw"></i> pt-BR</Link></li>
-            <li><Link to="#"><i class="fa fa-moon"></i> Dark </Link></li>
+            <Link to="/">Início</Link>
+            <Link to="/about">Currículo</Link>
+            <Link to="/projects">Projetos</Link>
+            <Link to="/blog">Blog</Link>
+            <Link to="#"><i class="fa fa-globe fa-fw"></i> pt-BR</Link>
+            <div class="theme-switch-wrapper">
+              <i class="fa fa-sun"></i>
+              <label class="theme-switch" for="checkbox">
+                  <input type="checkbox" id="checkbox" />
+                  <div class="slider round"></div>
+              </label>
+              <i class="fa fa-moon"></i>
+            </div>
           </ul>
           <div class="dropdown">
             <div class="dropbtn">
               <i class="fa fa-bars"></i>
             </div>
             <div class="dropdown-content">
-                <li><Link to="/">Início</Link></li>
-                <li><Link to="/about">Currículo</Link></li>
-                <li><Link to="/projects">Projetos</Link></li>
-                <li><Link to="/blog">Blog</Link></li>
-                <li><Link to="#"><i class="fa fa-globe fa-fw"></i> pt-BR</Link></li>
-                <li><Link to="#"><i class="fa fa-moon"></i> Dark </Link></li>
+                <Link to="/">Início</Link>
+                <Link to="/about">Currículo</Link>
+                <Link to="/projects">Projetos</Link>
+                <Link to="/blog">Blog</Link>
+                <Link to="#"><i class="fa fa-globe fa-fw"></i> pt-BR</Link>
             </div>
           </div>
         </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
         <section className={styles.corpo}>
           <h1>Oi, eu sou o Breno.</h1>
           <h2>Um graduando do Bacharelado em Física Computacional na USP (IFSC).</h2>
-          <p>Sou Técnico em Mecatrônica formado pelo Instituto Federal de São Paulo, Câmpus Catanduva.</p>
-          <p>Neste site, você poderá ver o meu <b><span class="blue">currículo</span></b> acadêmico e profissional, 
-          assim como os <b><span class="blue">projetos</span></b> que desenvolvi e outras produções em meu <b><span class="blue">blog.</span></b></p>
+          <h3>Sou Técnico em <b><span class="blue">Mecatrônica</span></b> formado pelo Instituto Federal de São Paulo, Câmpus Catanduva.</h3>
+          <h3>Neste site, você poderá ver o meu <b><span class="blue">currículo</span></b> acadêmico e profissional, 
+          assim como os <b><span class="blue">projetos</span></b> que desenvolvi e outras produções em meu <b><span class="blue">blog.</span></b></h3>
           <div className={styles.social}>
             <Link to="https://instagram.com/brenopelegrin"><i class="fab fa-instagram fa-2x"></i></Link>
             <Link to="https://facebook.com/brenopelegrin"><i class="fab fa-facebook fa-2x"></i></Link>
