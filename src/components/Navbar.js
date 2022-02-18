8b67ad0ab40df20989ca3b06ed90ed82a8b84122
Breno Henrique <breno.henrique@aluno.ifsp.edu.br>
2022-02-17 21:09:43 -0300

    Build before Deploy 2
    Added responsive menu
    
    CSS adapted for mobile devices

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,8 +15,18 @@ export default function Navbar() {
             <li><Link to="#"><i class="fa fa-globe fa-fw"></i> pt-BR</Link></li>
             <li><Link to="#"><i class="fa fa-moon"></i> Dark </Link></li>
           </ul>
-          <div className="menu">
-            <i class="fa fa-bars"></i>
+          <div class="dropdown">
+            <div class="dropbtn">
+              <i class="fa fa-bars"></i>
+            </div>
+            <div class="dropdown-content">
+                <li><Link to="/">Início</Link></li>
+                <li><Link to="/about">Currículo</Link></li>
+                <li><Link to="/projects">Projetos</Link></li>
+                <li><Link to="/blog">Blog</Link></li>
+                <li><Link to="#"><i class="fa fa-globe fa-fw"></i> pt-BR</Link></li>
+                <li><Link to="#"><i class="fa fa-moon"></i> Dark </Link></li>
+            </div>
           </div>
         </div>
     </nav>
--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
         <div className={styles.perfil}><img src="/breno.png" alt="Breno Henrique da Silva"></img></div>
         <section className={styles.corpo}>
           <h1>Oi, eu sou o Breno.</h1>
-          <h2>Um graduando do Bacharelado em Física Computacional na USP (IFSC)</h2>
+          <h2>Um graduando do Bacharelado em Física Computacional na USP (IFSC).</h2>
           <p>Sou Técnico em Mecatrônica formado pelo Instituto Federal de São Paulo, Câmpus Catanduva.</p>
           <p>Neste site, você poderá ver o meu <b><span class="blue">currículo</span></b> acadêmico e profissional, 
           assim como os <b><span class="blue">projetos</span></b> que desenvolvi e outras produções em meu <b><span class="blue">blog.</span></b></p>
